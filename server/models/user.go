2a6506f491ad88a85d535303e563083833645e9f
André Freitas <freitasandre38@gmail.com>
2023-06-09 18:48:53 -0300

    feat: new db querys and post routes

---
+++ server/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	Id        uint      `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"CreatedAt,omitempty"`
+}
