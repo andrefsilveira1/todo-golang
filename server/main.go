a7c49a192c2ea03405444c8d343e8d25b94c7a49
André Freitas <freitasandre38@gmail.com>
2023-06-10 18:33:25 -0300

    fix: remove useless routes

--- server/main.go
+++ server/main.go
@@ -41,52 +41,6 @@ func main() {
 		return c.JSON(datas)
 	})
 
-	// app.Get("/api/todos", func(c *fiber.Ctx) error {
-	// 	return c.JSON(todos)
-	// })
-
-	// app.Post("/api/todos", func(c *fiber.Ctx) error {
-	// 	todo := &Todo{}
-
-	// 	if err := c.BodyParser(todo); err != nil {
-	// 		return err
-	// 	}
-
-	// 	todo.ID = len(todos) + 1
-	// 	todos = append(todos, *todo)
-
-	// 	return c.JSON(todo)
-	// })
-
-	// app.Patch("/api/todos/:id/completed", func(c *fiber.Ctx) error {
-	// 	id, err := c.ParamsInt("id")
-	// 	if err != nil {
-	// 		return c.Status(401).SendString("Invalid ID:")
-	// 	}
-
-	// 	for i, t := range todos {
-	// 		if t.ID == id {
-	// 			todos[i].Completed = true
-	// 			break
-	// 		}
-	// 	}
-	// 	return c.JSON(todos)
-	// })
-
-	// app.Patch("/api/todos/:id/uncompleted", func(c *fiber.Ctx) error {
-	// 	id, err := c.ParamsInt("id")
-	// 	if err != nil {
-	// 		return c.Status(401).SendString("INVALID ID")
-	// 	}
-	// 	for i, t := range todos {
-	// 		if t.ID == id {
-	// 			todos[i].Completed = false
-	// 			break
-	// 		}
-	// 	}
-	// 	return c.JSON(todos)
-	// })
-
 	port := LoadEnv.LoadEnv("API_PORT")
 	fmt.Println("Listening on Port:", port)
 	log.Fatal(app.Listen(":" + port))
